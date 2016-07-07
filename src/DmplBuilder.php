108e745de39a4b02206893fa1e6869da2c42cb40
Niels Stampe <niels@keymade.io>
2016-07-07 02:32:44 +0200

    adds notice

--- src/DmplBuilder.php
+++ src/DmplBuilder.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of the Nielsiano\DmplBuilder package.
+ *
+ * (c) Niels Stampe <nielsstampe@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Nielsiano\DmplBuilder;
 
 /**
