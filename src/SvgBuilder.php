febe25181b909cdfd096f93c2ad3f12c0c7a0b58
Christian Rishøj <christian@rishoj.net>
2017-10-31 09:58:31 +0700

    add basic SVG builder

---
+++ src/SvgBuilder.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace Nielsiano\DmplBuilder;
+
+/**
+ * Illustrate a plot program visually as an SVG line drawing.
+ */
+class SvgBuilder implements PlotBuilder
+{
+    /**
+     * Current position.
+     */
+    protected $x = 0;
+    protected $y = 0;
+
+    protected $instructions = [];
+
+    protected $axesFlipped = false;
+    protected $penIsDown = true;
+    protected $tool = 'regular';
+
+    /**
+     * Adds a new plot of x and y to machine instructions.
+     */
+    public function plot(int $x, int $y): PlotBuilder
+    {
+
+        if ($this->axesFlipped) {
+            list($x, $y) = [$y, $x];
+        }
+
+        $targetX = $this->x + $x;
+        $targetY = $this->y + $y;
+
+        if ($this->penIsDown) {
+            $this->pushInstruction('line', [
+                'x1'    => $this->x,
+                'y1'    => $this->y,
+                'x2'    => $targetX,
+                'y2'    => $targetY,
+                'class' => $this->tool
+            ]);
+        }
+
+        $this->x = $targetX;
+        $this->y = $targetY;
+
+        return $this;
+    }
+
+    /**
+     * Changes the pen of the plotter.
+     */
+    public function changePen(int $pen): PlotBuilder
+    {
+        // TODO: Implement changePen() method.
+    }
+
+    /**
+     * Compiles a string in target format with machine instructions.
+     */
+    public function compile(): string
+    {
+        $instructions = implode("\n", $this->instructions);
+
+        return <<<SVG
+<svg xmlns="http://www.w3.org/2000/svg">
+    <defs>
+        <style>
+            line.regular {
+                stroke: rgb(255,0,0);
+                stroke-width: 2;
+            }
+
+            line.flex {
+                stroke: rgb(255,0,0);
+                stroke-width: 2;
+                stroke-dasharray: 10 2;
+            }
+        </style>
+    </defs>
+    {$instructions}
+</svg>
+SVG;
+    }
+
+    /**
+     * Pushes a command to the instructions.
+     */
+    public function pushCommand(string $command): PlotBuilder
+    {
+        // TODO: Implement pushCommand() method.
+    }
+
+    /**
+     * Lifts the pen up.
+     */
+    public function penUp(): PlotBuilder
+    {
+        $this->penIsDown = false;
+
+        return $this;
+    }
+
+    /**
+     * Pushes the pen down on paper.
+     */
+    public function penDown(): PlotBuilder
+    {
+        $this->penIsDown = true;
+
+        return $this;
+    }
+
+    /**
+     * Changes the plotter pen to use flexcut.
+     */
+    public function flexCut(): PlotBuilder
+    {
+        $this->tool = 'flex';
+
+        return $this;
+    }
+
+    /**
+     * Change to the regular plotter pen.
+     */
+    public function regularCut(): PlotBuilder
+    {
+        $this->tool = 'regular';
+
+        return $this;
+    }
+
+    /**
+     * Changes the pen pressure in gram.
+     */
+    public function pressure(int $gramPressure): PlotBuilder
+    {
+        // TODO: Simulate pressure with stroke width, perhaps?
+    }
+
+    /**
+     * Specifies measuring unit.
+     * 1 selects 0.001 inch
+     * 5 selects 0.005 inch
+     * M selects 0.1 mm
+     */
+    public function setMeasuringUnit($unit): PlotBuilder
+    {
+        // TODO: Implement setMeasuringUnit() method.
+    }
+
+    /**
+     * Changes the plotter velocity.
+     */
+    public function velocity(int $velocity): PlotBuilder
+    {
+        // TODO: Implement velocity() method.
+    }
+
+    /**
+     * Flips the x, y coordinates.
+     */
+    public function flipAxes(): PlotBuilder
+    {
+        $this->axesFlipped = true;
+
+        return $this;
+    }
+
+    /**
+     * Cuts off paper when a operation finishes.
+     */
+    public function cutOff(): PlotBuilder
+    {
+        // TODO: Implement cutOff() method.
+    }
+
+    protected function pushInstruction(string $name, array $parameters): PlotBuilder
+    {
+        $instruction = '<' . $name;
+        foreach ($parameters as $parameter => $value) {
+            $instruction .= ' ' . $parameter . '="' . htmlspecialchars($value) . '"';
+        }
+        $this->instructions[] = $instruction . ' />';
+
+        return $this;
+    }
+}
---
+++ tests/SvgBuilderTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Nielsiano\DmplBuilder\Tests;
+
+use Nielsiano\DmplBuilder\SvgBuilder;
+
+class SvgBuilderTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var SvgBuilder
+     */
+    protected $builder;
+
+    public function setUp()
+    {
+        $this->builder = new SvgBuilder();
+    }
+
+    public function test_it_can_draw_a_line()
+    {
+        $this->builder
+            ->plot(100, 200);
+
+        $this->assertContains('<line x1="0" y1="0" x2="100" y2="200" class="regular" />',
+            $this->builder->compile());
+    }
+
+    public function test_it_can_flip_axes()
+    {
+        $this->builder
+            ->flipAxes()
+            ->plot(100, 200);
+
+        $this->assertContains('<line x1="0" y1="0" x2="200" y2="100" class="regular" />',
+            $this->builder->compile());
+    }
+
+    public function test_it_will_not_output_plots_while_pen_is_up()
+    {
+        $this->builder
+            ->penUp()
+            ->plot(100, 200);
+        $this->assertNotContains('<line ', $this->builder->compile());
+    }
+
+    public function test_it_can_select_flex_cut()
+    {
+        $this->builder
+            ->flexCut()
+            ->plot(100, 200);
+
+        $this->assertContains('<line x1="0" y1="0" x2="100" y2="200" class="flex" />',
+            $this->builder->compile());
+    }
+
+}
